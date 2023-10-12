d8ce77c34b17326a2a9d46c3200327b4c0a06628
vikash <kumarvikasg56@gmail.com>
2023-10-12 21:00:53 +0530

    vikash_commit

--- src/App.js
+++ src/App.js
@@ -3,18 +3,15 @@ import './App.css';
 import Login from './component/Login';
 import { Card, CardBody } from 'reactstrap';
 
+console.log('msg Hello')
 function App() {
   return (
     <div >
       <Card className='whiteCard'>
+        
         <Navbar></Navbar>
         </Card>
-        <Login></Login>
-      
-      
-
-
-
+        <Login></Login>    
     </div>
   );
 }
